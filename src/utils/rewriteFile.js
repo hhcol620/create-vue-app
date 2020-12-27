c41f4a97ac7bba76bd6ebaca9a3f3c34389c42ac
hhcol <2838967868@qq.com>
2020-12-27 15:43:27 +0800

    初始化提交

---
+++ src/utils/rewriteFile.js
@@ -0,0 +1,17 @@
+const handlebars = require('handlebars')
+
+/**
+ * @params : filePath 文件路径 data 传入到文件中的对象
+ * @return : 将数据写入到模板文件
+ * @description :
+ * @date : 2020-12-27 15:01
+ * @author : hhcol_JS
+ */
+
+function rewriteFile(filePath, data) {
+  const fileContent = fs.readFileSync(filePath).toString()
+  const result = handlebars.compile(fileContent)(data)
+  fs.writeFileSync(filePath, result)
+}
+
+module.exports = { rewriteFile }
