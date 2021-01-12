abb31b3f05e29cf25ff3c4f5222efc74f5267c21
huanghe <haunghe@kanzhun.com>
2021-01-12 20:17:14 +0800

    日志merge

--- src/utils/inquirerList.js
+++ src/utils/inquirerList.js
@@ -1,16 +1,16 @@
 let questionList = [
-  // {
-  //   name: 'templateType',
-  //   message: 'which template type you need to create?',
-  //   type: 'list',
-  //   choices: ['PC', 'H5']
-  // },
   {
-    name: 'description',
+    name: 'templateType',
+    message: 'which template type you need to create?',
+    type: 'list',
+    choices: ['vue-template-pc', 'vue-template-h5']
+  },
+  {
+    name: 'Description',
     message: 'please enter a description:'
   },
   {
-    name: 'author',
+    name: 'Author',
     message: 'please enter a author:'
   }
 ]
