67e6607499fb56f60fe293d77821e2433cfaf874
huanghe <haunghe@kanzhun.com>
2021-01-12 21:07:03 +0800

    连通模板

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ program
         // start to download
         const spinner = ora('downloading template...')
         spinner.start()
-        const downloadPath = `direct:https://github.com/hhcol620/${answers.name}.git#master`
+        const downloadPath = `direct:https://github.com/hhcol620/${answers.templateType}.git#template-v1.0.0`
         download(downloadPath, name, { clone: true }, (err) => {
           if (err) {
             spinner.fail()
--- src/utils/inquirerList.js
+++ src/utils/inquirerList.js
@@ -1,20 +1,26 @@
 let questionList = [
-  {
-    name: 'templateType',
-    message: 'which template type you need to create?',
-    type: 'list',
-    choices: ['vue-template-pc', 'vue-template-h5']
-  },
-  {
-    name: 'description',
-    message: 'please enter a description:'
-  },
-  {
-    name: 'author',
-    message: 'please enter a author:'
-  }
-]
+    {
+        name: 'templateType',
+        message: 'which template type you need to create?',
+        type: 'list',
+        choices: ['vue-template-pc', 'vue-template-h5']
+    },
+    {
+        name: 'description',
+        message: 'please enter a description:'
+    },
+    {
+        name: 'author',
+        message: 'please enter a author:'
+    },
+    {
+        name: 'cssExpand',
+        message: 'which kind of CSS expansion do you want?',
+        type: 'list',
+        choices: ['less', 'scss', 'default']
+    }
+];
 
-let mapActions = {}
+let mapActions = {};
 
-module.exports = { questionList }
+module.exports = { questionList };
